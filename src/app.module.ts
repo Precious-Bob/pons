1608d17d55af7a0845c82d2bb26c8f7325092ba5
Precious Bob-Manuel <preciousbobmanuel501@gmail.com>
2026-08-26 20:07:49 +0100

    feat: integrate businesses module with CRUD functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { BusinessesModule } from "./businesses/businesses.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "better-sqlite3",
+        database: configService.get<string>("DB_PATH"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    BusinessesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
